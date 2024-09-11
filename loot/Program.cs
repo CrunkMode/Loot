a1223f716d57bbc3ebb5021d5f3126d51d16d913
Biomod <54336720+khoshekh1@users.noreply.github.com>
2024-09-11 17:36:10 -0400

    Test

--- loot/Program.cs
+++ loot/Program.cs
@@ -16,13 +16,16 @@ namespace loot
             {1, 0}, {2, 300}, {3, 900}, {4, 2700}, {5, 6500}, {6, 14000}, {7, 23000}, {8, 34000}, {9, 48000}, {10, 64000}
         };
 
+        //Player Data
         public static Player player = new Player();
         public static SaveData saveData = new SaveData();
         public static MetaStatistics metaStats = new MetaStatistics();
 
+        //NPC Data
         public static Alchemist Alchemist = new Alchemist();
         public static Blacksmith Blacksmith = new Blacksmith();
 
+        //Inventory Pointer
         public static Pointer pointer = new Pointer();
 
         //Main 
